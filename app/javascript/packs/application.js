d8e6271d3c056d1513c0e5ed4055a62e194ed367
M-Fukushima-vm <m.tech1980s@gmail.com>
2021-05-07 22:49:14 +0900

    準備まで

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,35 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// VueCLIでいう main.js
 
-Rails.start()
-ActiveStorage.start()
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import Vue from 'vue'
+import App from '../app.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+const vuetify = new Vuetify();
+// import router from '@/router'
+// import store from '@/store'
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    vuetify,
+    // router,
+    // store,
+    // created() {
+    //   const userString = localStorage.getItem('currentUser')
+    //   if (userString) {
+    //     const userData = JSON.parse(userString)
+    //     this.$store.commit('auth/SET_CURRENT_USER', userData)
+    //   }
+    // },
+    render: h => h(App)
+  }).$mount()
+  document.body.appendChild(app.$el)
+
+  console.log(app)
+})
