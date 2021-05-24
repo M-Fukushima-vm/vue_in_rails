254bd7e7678994582e2de79d0e3754d1fd65ded9
M-Fukushima-vm <m.tech1980s@gmail.com>
2021-05-24 22:30:47 +0900

    ユーザー詳細(バックエンド)まで

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   namespace :api do
-    resources :users, only:[:create]
+    resources :users, only:[:create, :show]
     resource :session, only:[:create, :destroy]
     resources :microposts, only:[:index, :create, :show, :update, :destroy]
     
